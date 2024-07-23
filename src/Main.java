96cfab9c9df84bac85fb5c17bfddceb029bb8eb9
Oleg <oastan@ya.ru>
2024-07-23 10:17:33 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("task 1");
+
+        String firstName = "Ivan";
+        String lastName = "Ivanov";
+        String middleName = "Ivanovich";
+
+        String fullName = firstName + " " + lastName + " " + middleName;
+        System.out.println("Ф.И.О сотрудника - " + fullName);
+
+        System.out.println();
+        System.out.println("task 2");
+        System.out.println(fullName.toUpperCase());
+
+        System.out.println();
+        System.out.println("task 3");
+
+        String fullName1 = "Иванов Семён Семёнович";
+        fullName1 = fullName1.replaceAll("ё", "е");
+        System.out.println("Данные Ф.И.О. сотрудника - " + fullName1);
+
+
+
+
 
     }
 }
